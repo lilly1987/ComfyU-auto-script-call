03cfbb338bac2a61b006eb99bf39aa17b56e863c
lilly <lilly1987@naver.com>
2026-05-23 11:25:48 +0900

    Update _run_my.cmd

--- _run_my.cmd
+++ _run_my.cmd
@@ -1,6 +1,6 @@
-"C:\Program Files\Mozilla Firefox\firefox.exe"
-"C:\Program Files\FreeFileSync\FreeFileSync.exe"
-"C:\Program Files\Notepad++\notepad++.exe"
-"C:\Program Files\OneCommander\OneCommander.exe"
-"C:\Program Files\Everything\Everything.exe"
-C:\Users\lilly\AppData\Local\GitHubDesktop\GitHubDesktop.exe
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
+start "" "C:\Program Files\FreeFileSync\FreeFileSync.exe"
+start "" "C:\Program Files\OneCommander\OneCommander.exe"
+start "" "C:\Program Files\Everything\Everything.exe"
+start "" C:\Users\lilly\AppData\Local\GitHubDesktop\GitHubDesktop.exe
+start "" "C:\Program Files\Notepad++\notepad++.exe"
